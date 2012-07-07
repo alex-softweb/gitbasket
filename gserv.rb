c6ac21651713376918cff91d49d95a99926ea08b
Alex Johnson <alex.johnson@softwebsolutions.com>
2012-07-07 18:37:02 +0530

    gserver

--- gserv.rb
+++ gserv.rb
@@ -7,24 +7,38 @@ class TimeServer < GServer
   end
 
   def serve(io)
-    io.puts(Time.now.to_s)
-    #while dec = io.getbyte
-      #puts dec
-    #end
-    #str = [hx].pack('H*')
-    %x[rm delete.rb]
-    while dec = io.getbyte
-      case dec
-      when 17
-        io.puts 'closing connection'
-        io.shutdown
-      when 20
-        io.puts '======output======'
-        io.puts %x[ruby delete.rb]
-        %x[rm delete.rb]
-      else
-        File.open('delete.rb', 'a') {|f| f.print dec.chr}
+    begin
+      io.puts(Time.now.to_s)
+      #io.print "\xff\xfb\x01"
+      io.print "\377\373\3" # send IAC WILL SUPPRESS-GOAHEAD
+      while dec = io.getbyte
+       case dec
+       #when 127
+         #io.print "\x8"
+       when 127
+         io.print "\b"
+         io.print "\b"
+       else
+         print dec.chr
+       end
       end
+      #%x[rm delete.rb]
+      #while dec = io.getbyte
+        #case dec
+        #when 17
+          #io.puts 'closing connection'
+          #io.shutdown
+        #when 20
+          #io.puts '======output======'
+          #io.puts %x[ruby delete.rb]
+          #io.puts '=================='
+          #%x[rm delete.rb]
+        #else
+          #File.open('delete.rb', 'a') {|f| f.print dec.chr}
+        #end
+      #end
+    rescue => e
+      print e
     end
   end
 end
@@ -33,3 +47,5 @@ server = TimeServer.new
 server.audit = true
 server.start
 server.join
+
+  #str = [hx].pack('H*')
